6a30cdf18ed67639a80535e23c623089fc206cef
U-DESKTOP-64LETAJ\Starw <starwarsfan2099@gmail.com>
2021-01-15 02:50:46 -0500

    Keep workin

--- commands.c
+++ commands.c
@@ -8,6 +8,7 @@ to the callbacks header. This file also includes the sql commands.
 
 ****************************************************************/
 
+#include <json.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -21,7 +22,7 @@ int command_get_database_path(){
 	char *profile_name;
 	char filepath[80];
 	FILE *catalog;
-	
+
 	// Get the current signed in username
 	profile_name = getenv("USERNAME");
 	printf("Profile Name: %s\n", profile_name);
@@ -35,14 +36,24 @@ int command_get_database_path(){
 
 	// Open the catalog file
  	if ((catalog = fopen(filepath, "r")) == NULL){
-       printf("Error! opening file");
+       printf("[-] Error opening catalog file");
 
        // Program exits if the file pointer returns NULL.
        return(1);
    	}
 
 	printf("[+] Opened Catalog file.\n");
+
+	fseek(catalog, 0, SEEK_END);
+	long fsize = ftell(catalog);
+	fseek(catalog, 0, SEEK_SET);
+
+	char *catalog_contents = malloc(fsize + 1);
+	fread(catalog_contents, 1, fsize, catalog);
 	fclose(catalog);
+
+	printf("[+] Catalog COntents:\n%s", catalog_contents);
+
 }
 
 #endif
